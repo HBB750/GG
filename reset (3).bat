108f2099d04023fd2e7a94a463a53b9537efed2b
HBB750 <moahana733@gmail.com>
2025-07-02 04:52:44 +0700

    Add files via upload

---
+++ reset (3).bat
@@ -0,0 +1,16 @@
+@echo off
+title Fake Mobile Model - GameLoop Spoof
+color 0a
+
+echo [+] Setting Mobile Model Spoof...
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceManufacturer /t REG_SZ /d Samsung /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceModel /t REG_SZ /d SM-G991B /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBrand /t REG_SZ /d Samsung /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceProduct /t REG_SZ /d o1sxxx /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDevice /t REG_SZ /d o1sxxx /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBoard /t REG_SZ /d kona /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceHardware /t REG_SZ /d qcom /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDevicePlatform /t REG_SZ /d android /f
+
+echo [+] All Done! Please restart your emulator.
+
