cc55627d73eeee2760bdd606cf7575f13edc71b7
HBB750 <moahana733@gmail.com>
2025-09-30 01:10:51 +0700

    Add files via upload

---
+++ TALM_ADVANCED_FIREWALL_BLOCKER.bat
@@ -0,0 +1,64 @@
+@echo off
+title T.A.L.M - ADVANCED FIREWALL BLOCKER (V2)
+color 0c
+echo [🔥] Starting advanced PUBG protection...
+echo.
+netsh advfirewall firewall add rule name="TALM_ADV_Block_43.156.202.110" dir=in  action=block remoteip=43.156.202.110 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_43.156.202.110" dir=out action=block remoteip=43.156.202.110 enable=yes
+echo [🚫] Blocked IP: 43.156.202.110
+netsh advfirewall firewall add rule name="TALM_ADV_Block_129.226.3.108" dir=in  action=block remoteip=129.226.3.108 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_129.226.3.108" dir=out action=block remoteip=129.226.3.108 enable=yes
+echo [🚫] Blocked IP: 129.226.3.108
+netsh advfirewall firewall add rule name="TALM_ADV_Block_106.55.130.242" dir=in  action=block remoteip=106.55.130.242 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_106.55.130.242" dir=out action=block remoteip=106.55.130.242 enable=yes
+echo [🚫] Blocked IP: 106.55.130.242
+netsh advfirewall firewall add rule name="TALM_ADV_Block_152.136.185.132" dir=in  action=block remoteip=152.136.185.132 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_152.136.185.132" dir=out action=block remoteip=152.136.185.132 enable=yes
+echo [🚫] Blocked IP: 152.136.185.132
+netsh advfirewall firewall add rule name="TALM_ADV_Block_49.51.26.25" dir=in  action=block remoteip=49.51.26.25 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_49.51.26.25" dir=out action=block remoteip=49.51.26.25 enable=yes
+echo [🚫] Blocked IP: 49.51.26.25
+netsh advfirewall firewall add rule name="TALM_ADV_Block_8.218.78.61" dir=in  action=block remoteip=8.218.78.61 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_8.218.78.61" dir=out action=block remoteip=8.218.78.61 enable=yes
+echo [🚫] Blocked IP: 8.218.78.61
+netsh advfirewall firewall add rule name="TALM_ADV_Block_8.210.203.206" dir=in  action=block remoteip=8.210.203.206 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_8.210.203.206" dir=out action=block remoteip=8.210.203.206 enable=yes
+echo [🚫] Blocked IP: 8.210.203.206
+netsh advfirewall firewall add rule name="TALM_ADV_Block_183.3.234.23" dir=in  action=block remoteip=183.3.234.23 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_183.3.234.23" dir=out action=block remoteip=183.3.234.23 enable=yes
+echo [🚫] Blocked IP: 183.3.234.23
+netsh advfirewall firewall add rule name="TALM_ADV_Block_150.138.234.76" dir=in  action=block remoteip=150.138.234.76 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_150.138.234.76" dir=out action=block remoteip=150.138.234.76 enable=yes
+echo [🚫] Blocked IP: 150.138.234.76
+netsh advfirewall firewall add rule name="TALM_ADV_Block_43.154.63.74" dir=in  action=block remoteip=43.154.63.74 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_43.154.63.74" dir=out action=block remoteip=43.154.63.74 enable=yes
+echo [🚫] Blocked IP: 43.154.63.74
+netsh advfirewall firewall add rule name="TALM_ADV_Block_183.3.225.11" dir=in  action=block remoteip=183.3.225.11 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_183.3.225.11" dir=out action=block remoteip=183.3.225.11 enable=yes
+echo [🚫] Blocked IP: 183.3.225.11
+netsh advfirewall firewall add rule name="TALM_ADV_Block_101.32.104.10" dir=in  action=block remoteip=101.32.104.10 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_101.32.104.10" dir=out action=block remoteip=101.32.104.10 enable=yes
+echo [🚫] Blocked IP: 101.32.104.10
+netsh advfirewall firewall add rule name="TALM_ADV_Block_183.3.234.56" dir=in  action=block remoteip=183.3.234.56 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_183.3.234.56" dir=out action=block remoteip=183.3.234.56 enable=yes
+echo [🚫] Blocked IP: 183.3.234.56
+netsh advfirewall firewall add rule name="TALM_ADV_Block_129.211.131.98" dir=in  action=block remoteip=129.211.131.98 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_129.211.131.98" dir=out action=block remoteip=129.211.131.98 enable=yes
+echo [🚫] Blocked IP: 129.211.131.98
+netsh advfirewall firewall add rule name="TALM_ADV_Block_116.85.57.203" dir=in  action=block remoteip=116.85.57.203 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_116.85.57.203" dir=out action=block remoteip=116.85.57.203 enable=yes
+echo [🚫] Blocked IP: 116.85.57.203
+netsh advfirewall firewall add rule name="TALM_ADV_Block_103.91.166.45" dir=in  action=block remoteip=103.91.166.45 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_103.91.166.45" dir=out action=block remoteip=103.91.166.45 enable=yes
+echo [🚫] Blocked IP: 103.91.166.45
+netsh advfirewall firewall add rule name="TALM_ADV_Block_203.205.253.4" dir=in  action=block remoteip=203.205.253.4 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_203.205.253.4" dir=out action=block remoteip=203.205.253.4 enable=yes
+echo [🚫] Blocked IP: 203.205.253.4
+netsh advfirewall firewall add rule name="TALM_ADV_Block_203.205.239.243" dir=in  action=block remoteip=203.205.239.243 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_203.205.239.243" dir=out action=block remoteip=203.205.239.243 enable=yes
+echo [🚫] Blocked IP: 203.205.239.243
+netsh advfirewall firewall add rule name="TALM_ADV_Block_123.151.71.34" dir=in  action=block remoteip=123.151.71.34 enable=yes
+netsh advfirewall firewall add rule name="TALM_ADV_Block_123.151.71.34" dir=out action=block remoteip=123.151.71.34 enable=yes
+echo [🚫] Blocked IP: 123.151.71.34
+echo.
+echo [✅] Advanced protection completed. PUBG tracking destroyed.
