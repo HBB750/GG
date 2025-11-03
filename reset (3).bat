20173fea888786dcba49eb9f49120a426ada8841
HBB750 <moahana733@gmail.com>
2025-11-03 08:32:45 -0800

    Update reset (3).bat

--- reset (3).bat
+++ reset (3).bat
@@ -1,21 +1,24 @@
 @echo off
-:: ==============================
-:: RedMagic 10 Pro - Direct Apply
-:: ==============================
-
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceManufacturer /t REG_SZ /d nubia /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceModel /t REG_SZ /d NX769J /f
+:: ✅ تعريف المحاكي كهاتف RedMagic 10 Ultra (Snapdragon 8 Gen 3)
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceManufacturer /t REG_SZ /d Nubia /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBrand /t REG_SZ /d REDMAGIC /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceProduct /t REG_SZ /d REDMAGIC_10_Pro /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDevice /t REG_SZ /d REDMAGIC_10_Pro /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceModel /t REG_SZ /d NX769J /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceProduct /t REG_SZ /d REDMAGIC_10_ULTRA /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDevice /t REG_SZ /d REDMAGIC_10_ULTRA /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBoard /t REG_SZ /d kalama /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceHardware /t REG_SZ /d qcom /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDevicePlatform /t REG_SZ /d android /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMFpsLevel /t REG_SZ /d 90 /f
 
-echo.
-echo ✅ تم تطبيق إعدادات REDMAGIC 10 Pro بنجاح!
-echo 🔁 أعد تشغيل GameLoop لتفعيل التغييرات.
+:: ⚙️ إعداد معدل الإطارات العالي
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMFpsLevel /t REG_SZ /d 120 /f
+
+:: 📱 تحسين أداء الألعاب
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMPerformanceMode /t REG_SZ /d HighPerformance /f
+
+echo ✅ تم تطبيق إعدادات RedMagic 10 Ultra بنجاح!
+
+
+
 
 
 
