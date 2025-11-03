69a715cb260220de2728e2929d9440f662ea22dd
HBB750 <moahana733@gmail.com>
2025-11-03 08:40:15 -0800

    Update reset (3).bat

--- reset (3).bat
+++ reset (3).bat
@@ -1,21 +1,21 @@
 @echo off
-:: ✅ تعريف المحاكي كهاتف RedMagic 10 Ultra (Snapdragon 8 Gen 3)
+:: ✅ تعريف المحاكي كهاتف RedMagic 9 Pro+ (Snapdragon 8 Gen 3)
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceManufacturer /t REG_SZ /d Nubia /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBrand /t REG_SZ /d REDMAGIC /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceModel /t REG_SZ /d NX769J /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceProduct /t REG_SZ /d REDMAGIC_10_ULTRA /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDevice /t REG_SZ /d REDMAGIC_10_ULTRA /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceProduct /t REG_SZ /d REDMAGIC_9_PRO_PLUS /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDevice /t REG_SZ /d REDMAGIC_9_PRO_PLUS /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBoard /t REG_SZ /d kalama /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceHardware /t REG_SZ /d qcom /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDevicePlatform /t REG_SZ /d android /f
 
-:: ⚙️ إعداد معدل الإطارات العالي
+:: ⚙️ إعداد معدل الإطارات العالي (120 FPS)
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMFpsLevel /t REG_SZ /d 120 /f
 
-:: 📱 تحسين أداء الألعاب
+:: 🚀 تحسين الأداء للألعاب
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMPerformanceMode /t REG_SZ /d HighPerformance /f
 
-echo ✅ تم تطبيق إعدادات RedMagic 10 Ultra بنجاح!
+echo ✅ تم تفعيل إعدادات RedMagic 9 Pro+ بنجاح!
 
 
 
