7a4400044c104a4e23faacfad111383f5bd3335a
HBB750 <moahana733@gmail.com>
2025-11-03 08:31:36 -0800

    Update reset (3).bat

--- reset (3).bat
+++ reset (3).bat
@@ -1,19 +1,24 @@
 @echo off
-title Fake Mobile Model - GameLoop Spoof
-color 0a
+:: ==============================
+:: RedMagic 10 Pro - Direct Apply
+:: ==============================
 
-echo [+] Setting Mobile Model Spoof...
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceManufacturer /t REG_SZ /d ASUS /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceModel /t REG_SZ /d ASUS_AI2401_B /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBrand /t REG_SZ /d ASUS /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceProduct /t REG_SZ /d ROG8 /f
-reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDevice /t REG_SZ /d ROG8 /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceManufacturer /t REG_SZ /d nubia /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceModel /t REG_SZ /d NX769J /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBrand /t REG_SZ /d REDMAGIC /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceProduct /t REG_SZ /d REDMAGIC_10_Pro /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDevice /t REG_SZ /d REDMAGIC_10_Pro /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBoard /t REG_SZ /d kalama /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceHardware /t REG_SZ /d qcom /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDevicePlatform /t REG_SZ /d android /f
 reg add "HKCU\Software\Tencent\MobileGamePC" /v VMFpsLevel /t REG_SZ /d 90 /f
 
-echo ✅ ASUS ROG Phone 8 profile applied!
+echo.
+echo ✅ تم تطبيق إعدادات REDMAGIC 10 Pro بنجاح!
+echo 🔁 أعد تشغيل GameLoop لتفعيل التغييرات.
+
+
+
 
 
 
