b2b76dd5cb6f3027dc764ed0af6b8d4723b46b6b
HBB750 <moahana733@gmail.com>
2025-11-27 01:22:00 -0300

    Add files via upload

---
+++ ne.bat
@@ -0,0 +1,17 @@
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceManufacturer /t REG_SZ /d Samsung /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBrand /t REG_SZ /d Samsung /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceModel /t REG_SZ /d SM-S928B /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceProduct /t REG_SZ /d S24Ultra /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDevice /t REG_SZ /d S24Ultra /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBoard /t REG_SZ /d kalama /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceHardware /t REG_SZ /d qcom /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDevicePlatform /t REG_SZ /d android /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceOSVersion /t REG_SZ /d 14 /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceCPU /t REG_SZ /d "Snapdragon 8 Gen 3 (Octa-core)" /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceRAM /t REG_SZ /d 12GB /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceResolution /t REG_SZ /d 3120x1440 /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceDensity /t REG_SZ /d 515 /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceGraphics /t REG_SZ /d "Adreno 750" /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceBatteryCapacity /t REG_SZ /d 5000mAh /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceRefreshRate /t REG_SZ /d 120Hz /f
+reg add "HKCU\Software\Tencent\MobileGamePC" /v VMDeviceStorage /t REG_SZ /d 1TB /f
